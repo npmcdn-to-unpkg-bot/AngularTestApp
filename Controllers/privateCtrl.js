cc819a201b855ca0fdb64b51c6d860b6ff268cb0
Marco De Vecchi <marco.devecchi84@gmail.com>
2016-04-18 20:21:59 +0200

    d

--- Controllers/privateCtrl.js
+++ Controllers/privateCtrl.js
@@ -9,19 +9,12 @@
 
     function PrivateCtrl(User, $location, $timeout) {
         var vm = this;
-<<<<<<< HEAD
-        var check=Authentification.checkLogin();
-        
-        if (check.value ==false) {
-            console.log(check.value ==false);
-            $location.path("/Registration");
-=======
+
         var check = User.checkLogin();
         if (check.value == false) {
             $timeout(function() {
                 $location.path('/Login');
             });
->>>>>>> 673f119178c2a5fcc8bbbc6d8b1a6e8bde62175d
         }
     }
 })();
